9218dfd8dc10278e1aaca153fb765af13ccd2698
Paige O'Connor <paige@wrng.ai>
2024-12-25 06:52:10 -0500

    install as current user

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@
 set -e
 
 # 1. Clone the package from GitHub
-if [ ! -d "/home/pi/p5ink" ]; then
-  git clone https://github.com/lostpaige/p5ink.git /home/pi/p5ink
+if [ ! -d "/home/$USER/p5ink" ]; then
+  git clone https://github.com/lostpaige/p5ink.git /home/$USER/p5ink
 else
-  echo "Repository already exists at /home/pi/p5ink. Pulling latest changes."
-  cd /home/pi/p5ink
+  echo "Repository already exists at /home/$USER/p5ink. Pulling latest changes."
+  cd /home/$USER/p5ink
   git pull
   cd -
 fi
 
 # 2. Create virtual environment and install requirements
-cd /home/pi/p5ink
+cd /home/$USER/p5ink
 
 # Create photos directory if it doesn't exist
 if [ ! -d "photos" ]; then
@@ -39,9 +39,9 @@ Description=P5InkyDiffusion Service
 After=network.target
 
 [Service]
-User=pi
-WorkingDirectory=/home/pi/p5ink
-ExecStart=/home/pi/p5ink/venv/bin/python /home/pi/p5ink/app.py
+User=$USER
+WorkingDirectory=/home/$USER/p5ink
+ExecStart=/home/$USER/p5ink/venv/bin/python /home/$USER/p5ink/app.py
 Restart=always
 
 [Install]
